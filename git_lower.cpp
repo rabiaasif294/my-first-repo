735df7ea4cd1bb69c7f61e47c42d10f63470e0df
rabiaasif294 <rabiaasif300600@gmail.com>
2025-03-26 20:30:18 +0500

    Add files via upload

---
+++ git_lower.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class Solution {
+public:
+    string toLowerCase(string s) {
+        for (int i=0; i<s.size(); i++) {
+            if(s[i]<=90 && s[i]>=65) {
+                s[i] = s[i] + 32;
+                
+            }
+        }
+        return s;
+    }
+};
